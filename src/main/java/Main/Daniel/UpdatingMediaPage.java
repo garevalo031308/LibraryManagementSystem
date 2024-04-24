83cc4f25af2fd633604c6591eedda0f2a93dbcfa
Daniel <126022409+DanielGonzalezGil@users.noreply.github.com>
2024-04-23 21:44:59 -0400

    updated UpdatingMediaPage.java

--- src/main/java/Main/Daniel/UpdatingMediaPage.java
+++ src/main/java/Main/Daniel/UpdatingMediaPage.java
@@ -9,11 +9,13 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class UpdatingMediaPage extends Application {
     public static void main(String[] args){
@@ -33,8 +35,7 @@ public class UpdatingMediaPage extends Application {
 
         //LibraHub logo
         ImageView logo = new ImageView();
-        //TODO Figure out how to get the image to show up
-        Image image = new Image("https://github.com/garevalo031308/LibraryMangementSystem/blob/ac0fbfd91aae86a2d204f75a0fc0337a75683193/src/main/resources/Images/Main/libgenlogo.png");
+        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/Images/Main/libgenlogo.png")));
         logo.setImage(image);
         logo.setFitHeight(120);
         logo.setFitWidth(118);
@@ -82,6 +83,43 @@ public class UpdatingMediaPage extends Application {
         loginLabel.setFont(Font.font(13));
         loginLabel.setUnderline(true);
 
+        //TODO: Add "Updating Media" label
+        //TODO: Add Update media description ("Click the button below to add or remove media to the Library catalog" )
+
+        //Add media button
+        //TODO: Figure out how to chnage text size
+        Button addMediaButton = new Button("Add Media");
+        addMediaButton.setLayoutX(450);
+        addMediaButton.setLayoutY(270);
+        addMediaButton.setPrefSize(150, 50);
+        addMediaButton.setStyle("-fx-background-color: #6DA6C5");
+
+        //Delete media button
+        //TODO: Figure out how to chnage text size
+        Button deleteMediaButton = new Button("Delete Media");
+        deleteMediaButton.setLayoutX(870);
+        deleteMediaButton.setLayoutY(270);
+        deleteMediaButton.setPrefSize(150, 50);
+        deleteMediaButton.setStyle("-fx-background-color: #FF5A5F");
+
+        //pane where radio buttons and input text fields will be placed
+        //TODO: Figure out spacing on top of pane to fit buttons nicely
+        Pane bodyPane = new Pane();
+        bodyPane.setPrefWidth(675);
+        bodyPane.setPrefHeight(600);
+        bodyPane.setLayoutX(400);
+        bodyPane.setLayoutY(240);
+        bodyPane.setStyle("-fx-background-color: #F7DFB6");
+
+        //TODO: Add radio buttons for media type (Book, E-Book)
+        //TODO: Add text fields for media title, author, genre, ISBN, and publication date
+
+
+
+
+
+
+
 
 
         //---Body content for Create Account Page---//
@@ -90,8 +128,10 @@ public class UpdatingMediaPage extends Application {
 
 
         stage.setTitle("Library Management System");// sets current scene
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/Images/Main/libgenlogo.png")))); //sets icon
         root.getChildren().addAll(header, logo, title, account); //adds header to the root (children are the modules)
-        root.getChildren().addAll(catalog, aboutus, loginLabel);
+        root.getChildren().addAll(catalog, aboutus, loginLabel, bodyPane, addMediaButton); //adds buttons to the root
+        root.getChildren().addAll(deleteMediaButton);
         stage.setScene(scene);
         stage.show();
 
