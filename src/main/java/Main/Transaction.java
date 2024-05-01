d018b232c8b7b2b7146589e2857776ddf5bcf11d
garevalo031308 <g.arevalo031308@gmail.com>
2024-04-30 23:19:04 -0400

    Update Transaction.java

--- src/main/java/Main/Transaction.java
+++ src/main/java/Main/Transaction.java
@@ -8,15 +8,44 @@ public class Transaction {
     private final StringProperty author;
     private final StringProperty dateCheckedOut;
     private final StringProperty dateDue;
-
+    private final StringProperty bookID;
+    private final StringProperty transactionID;
     private final StringProperty status;
 
-    public Transaction(String title, String author, String dateCheckedOut, String dateDue, StringProperty status){
+    public Transaction(String title, String author, String dateCheckedOut, String dateDue, StringProperty bookID, StringProperty transactionID, StringProperty status){
         this.title = new SimpleStringProperty(title);
         this.author = new SimpleStringProperty(author);
         this.dateCheckedOut = new SimpleStringProperty(dateCheckedOut);
         this.dateDue = new SimpleStringProperty(dateDue);
+        this.bookID = bookID;
+        this.transactionID = transactionID;
         this.status = status;
+    }
+
+    public Transaction(String title, String author, String dateCheckedOut, String dateDue, SimpleStringProperty bookID) {
+        this.title = new SimpleStringProperty(title);
+        this.author = new SimpleStringProperty(author);
+        this.dateCheckedOut = new SimpleStringProperty(dateCheckedOut);
+        this.dateDue = new SimpleStringProperty(dateDue);
+        this.bookID = bookID;
+        this.transactionID = new SimpleStringProperty("0");
+        this.status = new SimpleStringProperty("0");
+    }
+
+    public String getBookID() {
+        return bookID.get();
+    }
+
+    public StringProperty bookIDProperty() {
+        return bookID;
+    }
+
+    public String getTransactionID() {
+        return transactionID.get();
+    }
+
+    public StringProperty transactionIDProperty() {
+        return transactionID;
     }
 
     public String getTitle() {
