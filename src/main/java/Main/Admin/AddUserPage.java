694ceca846b4561ce0d121fe6c83a6ce3f8a6a4f
garevalo031308 <g.arevalo031308@gmail.com>
2024-05-01 18:05:34 -0400

    Update AddUserPage.java

--- src/main/java/Main/Admin/AddUserPage.java
+++ src/main/java/Main/Admin/AddUserPage.java
@@ -1,17 +1,10 @@
 package Main.Admin;
 
 import Main.*;
-import Main.Media.CatalogPage;
-import Main.User.AccountPage;
-import Main.User.CheckoutPage;
-import Main.User.LoginPage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -20,10 +13,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Objects;
 import java.util.Random;
-
-import static Main.HomePage.currentLoggedInUser;
 
 public class AddUserPage {
 
@@ -32,87 +22,7 @@ public class AddUserPage {
         Scene scene = new Scene(root, 1280, 900);
         scene.setFill(Paint.valueOf("#F4CE90"));
 
-        Rectangle header = new Rectangle();//new rectangle within the stage
-        header.setWidth(1280); //set width
-        header.setHeight(132); //set height
-        header.setFill(Paint.valueOf("#FF5A5F"));
-
-        ImageView logo = new ImageView(); //new image view
-        Image logoimg = new Image(Objects.requireNonNull(AdminPage.class.getResourceAsStream("/Images/Main/libgenlogo.png"))); //get image from the path
-        logo.setImage(logoimg); //set image
-        logo.setFitHeight(124);
-        logo.setFitWidth(122);
-        logo.setLayoutX(8);
-        logo.setLayoutY(6);
-
-        Label title = new Label("LibraHub");
-        title.setLayoutX(175);
-        title.setLayoutY(15);
-        title.setFont(Font.font(80));
-
-        Button account = new Button("Account");
-        account.setLayoutX(553);
-        account.setLayoutY(41);
-        account.setPrefWidth(109);
-        account.setPrefHeight(67);
-        account.setTextFill(Paint.valueOf("white"));
-        account.setStyle("-fx-background-color:  #363732");
-        account.setOnAction(e->{
-            if (currentLoggedInUser.isEmpty()) {
-                LoginPage.loginPage(stage);
-            } else {
-                AccountPage.accountPage(stage, currentLoggedInUser);
-            }
-        });
-
-        Button catalog = new Button("Catalog");
-        catalog.setStyle("-fx-background-color: #363732");
-        catalog.setTextFill(Paint.valueOf("white"));
-        catalog.setPrefWidth(109);
-        catalog.setPrefHeight(67);
-        catalog.setLayoutX(708);
-        catalog.setLayoutY(41);
-        catalog.setOnAction(e-> CatalogPage.catalogPage(stage, ""));
-
-        Button aboutus = new Button("About Us");
-        aboutus.setStyle("-fx-background-color: #363732");
-        aboutus.setTextFill(Paint.valueOf("white"));
-        aboutus.setPrefHeight(67);
-        aboutus.setPrefWidth(109);
-        aboutus.setLayoutX(863);
-        aboutus.setLayoutY(41);
-        aboutus.setOnAction(e-> AboutUsPage.aboutUsPage(stage));
-
-        Label loginLabel = new Label("Log In");
-        loginLabel.setLayoutX(1164);
-        loginLabel.setLayoutY(6);
-        loginLabel.setFont(Font.font(13));
-        loginLabel.setUnderline(true);
-        loginLabel.setOnMouseClicked(e-> LoginPage.loginPage(stage));
-
-        ImageView cartimage = new ImageView();
-        Image cart = new Image(Objects.requireNonNull(AccountPage.class.getResourceAsStream("/Images/Main/cart.png")));
-        cartimage.setImage(cart);
-        cartimage.setFitWidth(90);
-        cartimage.setFitHeight(59);
-        cartimage.setLayoutX(1206);
-        cartimage.setOnMouseClicked(e -> {
-            CheckoutPage.checkoutPage(stage, "4440486");
-        });
-
-        TextField searchBar = new TextField();
-        searchBar.setPromptText("Type a title, author, etc. here");
-        searchBar.setPrefSize(226, 26);
-        searchBar.setLayoutX(991);
-        searchBar.setLayoutY(83);
-
-        Button searchButton = new Button("Search");
-        searchButton.setLayoutX(1217);
-        searchButton.setLayoutY(83);
-        searchButton.setOnAction(e -> {
-            String searchQuery = searchBar.getText();
-            CatalogPage.catalogPage(stage, searchQuery);
-        });
+        Header.getHeader(stage, root);
 
         Text addTitle = new Text(482, 186, "Add User");
         addTitle.setFont(Font.font(60));
@@ -204,11 +114,20 @@ public class AddUserPage {
         typeText.setFont(Font.font(35));
 
         stage.setScene(scene);
-        root.getChildren().addAll(header, logo, title, account, catalog, aboutus, loginLabel, cartimage, searchBar, searchButton);
         root.getChildren().addAll(addTitle, clickText, idField, firstNameField, lastNameField, passwordField, emailField, customerRadioButton, adminRadioButton, librarianRadioButton, addButton, backButton, idText, firstNameText, lastNameText, passwordText, emailText, typeText);
         stage.show();
 
+        addButton.setOnAction(e -> {
+            String id = idField.getText();
+            String firstName = firstNameField.getText();
+            String lastName = lastNameField.getText();
+            String password = passwordField.getText();
+            String email = emailField.getText();
+            RadioButton selectedRadioButton = (RadioButton) userType.getSelectedToggle();
+            String type = selectedRadioButton.getText();
 
+            addUserToDatabase(id, firstName, lastName, password, email, type);
+        });
     }
 
     private static String createRandomID(){
@@ -223,21 +142,36 @@ public class AddUserPage {
         return id.toString();
     }
 
+    public static void addUserToDatabase(String id, String firstName, String lastName, String password, String email, String type) {
+        try (Connection connection = HomePage.getConnection()){
+            String SQL = "INSERT INTO user (id, first_name, last_name, password, email, role) VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(SQL);
+            statement.setString(1, id);
+            statement.setString(2, firstName);
+            statement.setString(3, lastName);
+            statement.setString(4, password);
+            statement.setString(5, email);
+            statement.setString(6, type);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     private static boolean checkUserID(String id){
         try (Connection connection = HomePage.getConnection()){
-            String[] tables = {"customer", "admin", "librarian"};
-            for (String table : tables) {
-                String SQL = "SELECT COUNT(*) FROM " + table + " WHERE id = ?";
-                PreparedStatement statement = connection.prepareStatement(SQL);
-                statement.setString(1, id);
-                ResultSet resultSet = statement.executeQuery();
-                if (resultSet.next()){
-                    int count = resultSet.getInt(1);
-                    if (count > 0) {
-                        return true;
-                    }
+
+            String SQL = "SELECT COUNT(*) FROM user WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(SQL);
+            statement.setString(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()){
+                int count = resultSet.getInt(1);
+                if (count > 0) {
+                    return true;
                 }
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
