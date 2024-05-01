23cce844ef64fe3508336371a8de5818785a9acf
garevalo031308 <g.arevalo031308@gmail.com>
2024-05-01 18:05:57 -0400

    Update HomePage.java

--- src/main/java/Main/HomePage.java
+++ src/main/java/Main/HomePage.java
@@ -1,9 +1,6 @@
 package Main;
 
 import Main.Media.CatalogPage;
-import Main.User.AccountPage;
-import Main.User.CheckoutPage;
-import Main.User.LoginPage;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -13,7 +10,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -24,9 +20,9 @@ import java.sql.SQLException;
 import java.util.Objects;
 
 // STRIKE link up all pages together
-// TODO log in and save user while they are browsing
-// TODO logout function
-// TODO Remove cart from user when they log out/close application
+// STRIKE log in and save user while they are browsing
+// STRIKE logout function
+// STRIKE Remove cart from user when they log out/close application
 // TODO make sure all buttons do something
 // STRIKE search bar on all pages
 // TODO change cart image with each time they add something to cart
@@ -34,6 +30,8 @@ import java.util.Objects;
 // TODO back buttons on certain pages
 // TODO a way for librarian to create accounts for customers
 // TODO allow librarians to also have a customer account and have same functions as a customer
+// TODO stage titles for each page
+// TODO logos for each page
 
 // TODO Fix admin, librarian, and customer database
 // STRIKE Database for borrowed books that is linked to user that contains the book, return date, etc.
@@ -47,12 +45,13 @@ import java.util.Objects;
 
 // STRIKE - CreateAccountPage: allow users to create an account through application
 // STRIKE - CreateAccountPage: after adding a user go to their account page
-// TODO - Catalog Page: Fix filters, allow for searcing by title, author, genre, etc. on the page, and resetting of filters
+// STRIKE - Catalog Page: Fix filters, allow for searcing by title, author, genre, etc. on the page, and resetting of filters
 // STRIKE - Login page: allow users to login with their credentials or create an account
 // STRIKE - Admin page: create users, have some type of button that allows admins to login with their credentials and open something up
-// TODO /\ edit users, delete users, etc.
+// STRIKE /\ edit users, delete users, etc.
+// TODO - Books popup into an actual page with back button
 // STRIKE - Updating Media Page: allow librarians to update media information (extra button if logged in as librarian?)
-// TODO /\ a functions page that allows them to do certain things depending on their role, then a button that gets added on home page to access it
+// STRIKE /\ a functions page that allows them to do certain things depending on their role, then a button that gets added on home page to access it
 // STRIKE - AboutUs Page: add some extra fluff like a google maps thing or whatever
 // STRIKE - UserLookUp Page: allow librarians to look up users and see what they have checked out
 // STRIKE - User Page: allow users to see what they have checked out (Need to make in scene builder)
@@ -61,7 +60,6 @@ import java.util.Objects;
 public class HomePage extends Application {
 
     public static String url = "jdbc:mysql://localhost:3306/librahub";
-    public static String currentLoggedInUser = "";
 
 public static void main(String[] args){
     try {
@@ -91,74 +89,6 @@ public static void main(String[] args){
         Group root = new Group(); //group is groups of module(containers, test fields)
         Scene scene = new Scene(root, 1280,  900); //scene of page, creating width, and height (x,y value)
         scene.setFill(Paint.valueOf("#F4CE90")); //set
-
-        Rectangle header = new Rectangle();//new rectangle within the stage
-        header.setWidth(1280); //set width
-        header.setHeight(132); //set height
-        header.setFill(Paint.valueOf("#FF5A5F"));
-
-        ImageView logo = new ImageView(); //new image view
-        Image img = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/Images/Main/libgenlogo.png"))); //get image from the path
-        logo.setImage(img); //set image
-        logo.setFitHeight(124);
-        logo.setFitWidth(122);
-        logo.setLayoutX(8);
-        logo.setLayoutY(6);
-
-        Label title = new Label("LibraHub");
-        title.setLayoutX(175);
-        title.setLayoutY(15);
-        title.setFont(Font.font(80));
-
-        Button account = new Button("Account");
-        account.setLayoutX(553);
-        account.setLayoutY(41);
-        account.setPrefWidth(109);
-        account.setPrefHeight(67);
-        account.setTextFill(Paint.valueOf("white"));
-        account.setStyle("-fx-background-color:  #363732");
-        account.setOnAction(e->{
-            if (currentLoggedInUser.isEmpty()) {
-                LoginPage.loginPage(stage);
-            } else {
-                AccountPage.accountPage(stage, currentLoggedInUser);
-            }
-        });
-
-        Button catalog = new Button("Catalog");
-        catalog.setStyle("-fx-background-color: #363732");
-        catalog.setTextFill(Paint.valueOf("white"));
-        catalog.setPrefWidth(109);
-        catalog.setPrefHeight(67);
-        catalog.setLayoutX(708);
-        catalog.setLayoutY(41);
-        catalog.setOnAction(e-> CatalogPage.catalogPage(stage, ""));
-
-        Button aboutus = new Button("About Us");
-        aboutus.setStyle("-fx-background-color: #363732");
-        aboutus.setTextFill(Paint.valueOf("white"));
-        aboutus.setPrefHeight(67);
-        aboutus.setPrefWidth(109);
-        aboutus.setLayoutX(863);
-        aboutus.setLayoutY(41);
-        aboutus.setOnAction(e-> AboutUsPage.aboutUsPage(stage));
-
-        Label loginLabel = new Label("Log In");
-        loginLabel.setLayoutX(1164);
-        loginLabel.setLayoutY(6);
-        loginLabel.setFont(Font.font(13));
-        loginLabel.setUnderline(true);
-        loginLabel.setOnMouseClicked(e-> LoginPage.loginPage(stage));
-
-        ImageView cartimage = new ImageView();
-        Image cart = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/Images/Main/cart.png")));
-        cartimage.setImage(cart);
-        cartimage.setFitWidth(90);
-        cartimage.setFitHeight(59);
-        cartimage.setLayoutX(1206);
-        cartimage.setOnMouseClicked(e -> {
-            CheckoutPage.checkoutPage(stage, "4440486");
-        });
 
         ImageView banner = new ImageView();
         Image imgbanner = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/Images/HomePage/banner.jpg")));
@@ -238,9 +168,10 @@ public static void main(String[] args){
 
         stage.setTitle("Library Management System");// sets current scene
         stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/Images/Main/libgenlogo.png")))); //sets icon
-        root.getChildren().addAll(header, logo, title, account, catalog, aboutus, loginLabel, cartimage, banner, helpLabel, searchbar, searchbutton); //adds header to the root (children are the modules)
+        root.getChildren().addAll(banner, helpLabel, searchbar, searchbutton); //adds header to the root (children are the modules)
         root.getChildren().addAll(popularLabel);
         root.getChildren().addAll(popular1, popular2, popular3, popular4, popular5);
+        Header.getHomeHeader(stage, root);
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
